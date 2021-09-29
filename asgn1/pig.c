a9d413d5fbd29fb6cafd67a8583fbfae5a6d0342
Oliver Hu <oehu@ucsc.edu>
2021-09-29 18:40:18 +0000

    progress towards creating players and scores

--- asgn1/pig.c
+++ asgn1/pig.c
@@ -3,7 +3,11 @@
 
 int main(void) {
 	int players;
+	int scores[10]= {0,0,0,0,0,0,0,0,0,0};
+		enum Position { SIDE, SIDE, RAZORBACK, TROTTER, SNOUTER, JOWLER, JOWLER};
 	printf("How many players will play: ");
 	scanf("%d",&players);
+	printf("%d players\n", players);
+	while(
 	return 0;
 	}
