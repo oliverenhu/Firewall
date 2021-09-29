9483e5694fd8379cc9653c53d66555a5581de467
Oliver Hu <oehu@ucsc.edu>
2021-09-29 22:18:50 +0000

    progress towards asking for players

--- asgn1/pig.c
+++ asgn1/pig.c
@@ -2,12 +2,18 @@
 #include <stdlib.h>
 
 int main(void) {
-	int players;
-	int scores[10]= {0,0,0,0,0,0,0,0,0,0};
-		enum Position { SIDE, SIDE, RAZORBACK, TROTTER, SNOUTER, JOWLER, JOWLER};
-	printf("How many players will play: ");
-	scanf("%d",&players);
-	printf("%d players\n", players);
-	while(
-	return 0;
-	}
+        int players;
+        int scores[10]= {0,0,0,0,0,0,0,0,0,0};
+        typedef enum { SIDE, RAZORBACK, TROTTER, SNOUTER, JOWLER} Position;
+        const Position pig[7]= { SIDE, SIDE , RAZORBACK, TROTTER, SNOUTER, JOWLER, JOWLER} ;
+        printf("How many players will play: ");
+        scanf("%d",&players);
+        if(players>1 && players<11 ){
+            printf("%d players\n", players);
+        }
+        else {
+            fprintf ( stderr , " Invalid number of players . Using 2 instead .\n");
+
+        }
+        return 0;
+        }
