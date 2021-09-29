baf665676acca33b7f9d2d71306d2deebf024b56
Oliver Hu <oehu@ucsc.edu>
2021-09-29 23:21:47 +0000

    adding header file names.h

--- asgn1/pig.c
+++ asgn1/pig.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <names.h>
 int main(void) {
-        int players;
+        int players,seed;
         int scores[10]= {0,0,0,0,0,0,0,0,0,0};
         typedef enum { SIDE, RAZORBACK, TROTTER, SNOUTER, JOWLER} Position;
         const Position pig[7]= { SIDE, SIDE , RAZORBACK, TROTTER, SNOUTER, JOWLER, JOWLER} ;
-        printf("How many players will play: ");
+        printf("How many players ? ");
         scanf("%d",&players);
         if(players>1 && players<11 ){
             printf("%d players\n", players);
@@ -15,5 +15,10 @@ int main(void) {
             fprintf ( stderr , " Invalid number of players . Using 2 instead .\n");
 
         }
-        return 0;
+        printf("Random seed:");
+        scanf("%d",&seed);
+        if(seed != 2021 ){
+            fprintf ( stderr , " Invalid random seed . Using 2021 instead .\n");
         }
+        
+        return 0;
