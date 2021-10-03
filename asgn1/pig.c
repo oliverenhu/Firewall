7d43477f03eee920e3991a9bc1eb4a0a75652d20
Oliver Hu <oehu@ucsc.edu>
2021-10-03 02:51:01 +0000

    added new function check_winner and constant get_position

--- asgn1/pig.c
+++ asgn1/pig.c
@@ -3,12 +3,21 @@
 #include "names.h"
 typedef enum { SIDE=0, RAZORBACK=10, TROTTER=10, SNOUTER=15, JOWLER=5} Position;
 const Position pig[7]= { SIDE, SIDE , RAZORBACK, TROTTER, SNOUTER, JOWLER, JOWLER} ;
+const char* get_position(int points){
+    switch (points){
+        case 0: return "side";
+        case 1: return "side";
+        case 2: return "razorback";
+        case 3: return "trotter";
+        case 4: return "snouter";
+        case 5: return "jowler";
+        case 6: return "jowler";
+    }
+}
+void check_winner(int players);
 int main(void) {
         int players;
 	long long seed;
-       // int scores[10]={0,0,0,0,0,0,0,0,0,0};
-	
-        
         printf("How many players ? ");
         scanf("%d",&players);
         if(players>1 && players<11 ){
@@ -22,17 +31,42 @@ int main(void) {
         scanf("%lld",&seed);
         if(seed <= 4294967295 && seed >=0 ){
             srandom(seed);
-            printf (" - generated %lu\n", random () );
- 
+	    check_winner(players);
         }
         else{
             fprintf ( stderr , " Invalid random seed . Using 2021 instead .\n");
 	    srandom(2021);
-            printf (" - generated %lu\n", random () );
+            check_winner(players);
         }
-	for(int i = 0;i<10;i=i+1){
-        printf("%s\n",names[i]);
-        }
-    
         return 0;
+
+}
+void check_winner(int players){
+    int scores[10]={0,0,0,0,0,0,0,0,0,0};
+    int win=0;
+    WIN:while(win==0){
+        for(int i=0;i<players;i=i+1){
+            int position;
+            int r;
+            printf("%s rolls the pig... ",names[i]);
+            do {
+                if(scores[i]>=100){
+                    printf("%s wins with %d\n",names[i],scores[i]);
+                    win=1;
+                    goto WIN;
+
+                }
+                r=random()%7;
+                position=pig[r];
+                scores[i]+=position;
+                if(r==0||r==1){
+                    printf("pig lands on %s\n",get_position(r));
+                }
+                else{
+                    printf("pig lands on %s ",get_position(r));
+                }
+            }while(position!=0);
+
+        }
+    }
 }
