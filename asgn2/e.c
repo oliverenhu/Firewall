37a573eb43bb49610c6c01407113fa3ead6117c0
Oliver Hu <oehu@ucsc.edu>
2021-10-07 20:08:46 +0000

    linked files

---
+++ asgn2/e.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "mathlib.h"
+
+double e(){
+	printf("hi\n");
+	return 0;
+}
--- asgn2/math-test.c
+++ asgn2/math-test.c
@@ -1,20 +1,21 @@
-# include < stdio .h >
-# include < unistd .h >
-
+# include <stdio.h>
+# include <unistd.h>
+# include "mathlib.h"
 # define OPTIONS "pi:"
 
 int main ( int argc , char ** argv ) {
-int opt = 0;
-while (( opt = getopt (argc , argv , OPTIONS )) != -1) {
-switch ( opt ) {
-case 'p':
-printf ("-p option .\n");
-break ;
-case 'i':
-printf ("-i option : %s is parameter .\n", optarg ) ;
-break ;
-}
-}
-return 0;
-}
+	int opt = 0;
+	while (( opt = getopt (argc , argv , OPTIONS )) != -1) {
+		switch ( opt ) {
+		case 'p':
+		printf ("-p option .\n");
+		e();
+		break;
+		case 'i':
+		printf ("-i option : %s is parameter .\n", optarg ) ;
+		break ;
+		}
+		}	
+	return 0;
+	}
 
