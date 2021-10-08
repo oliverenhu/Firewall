03af10a95a894495329d11b716bc3d1fae3f4c84
Oliver Hu <oehu@ucsc.edu>
2021-10-08 04:48:19 +0000

    progress on conculating e

--- asgn2/e.c
+++ asgn2/e.c
@@ -1,11 +1,33 @@
 #include <stdio.h>
 #include "mathlib.h"
 
+int e_terms();
 double e(){
-	printf("hi\n");
+	double error;
+	double term = 1;
+	double e = 1.0000000000000;
+	double preterm=1;
+	double pree;
+	double cont=1;
+	do{
+		pree=e;
+		printf("pree %.15f\n",pree);
+		e+=cont/(term*preterm);
+		printf("e %.15f\n",e);
+		preterm=term*preterm;
+	        printf("preterm %.15f\n",preterm);
+		printf("term %f\n",term);
+		term+=1;
+		error=e-pree;
+		printf("error %.15f\n",error);
+	}while(error>EPSILON);
+	printf("%f\n",term);
+	printf("%.15f\n",e);
 	return 0;
 }
-int e_terms(){
-return 0;
-}
+/*int e_terms(){
+	printf("%d",term;
+
+	return 0;
+}*/
 
