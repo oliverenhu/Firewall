844a465fea6760fcaf1e3b1be72683a0711e23c5
Oliver Hu <oehu@ucsc.edu>
2021-10-09 21:16:17 +0000

    fixed newton

--- asgn2/newton.c
+++ asgn2/newton.c
@@ -1,14 +1,25 @@
 #include <stdio.h>
 #include "mathlib.h"
-
+double fabsminus(double y,double z);
 double sqrt_newton(double x){
 	double z = 0.0;
 	double y = 1.0;
-	while((y-z) > EPSILON){
+	while(fabsminus(y,z) > EPSILON){
 	z = y;
 	y = 0.5 * (z + x / z);
 	}
 	
 	printf("%f",y);
 	return y;
+}
+double fabsminus(double y,double z){
+	double zero = 0;
+	if(y-z>zero){
+		return y-z;
+	}
+	else{
+	return (-1*(y-z));	
+
+
+}
 }
