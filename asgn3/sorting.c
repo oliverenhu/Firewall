65960dbd80e06536c45b53963a0bc788904214c9
Oliver Hu <oehu@ucsc.edu>
2021-10-16 01:06:11 +0000

    added sets

--- asgn3/sorting.c
+++ asgn3/sorting.c
@@ -9,54 +9,64 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <unistd.h>
-#define OPTIONS "aeisqrnph:"
+#define OPTIONS "aeisqr:n:p:h:"
+typedef enum { HEAP , SHELL , INSERTION , QUICK } Sorts;
+const char *names[] = {"Heap Sort","Shell Sort","Insertion Sort","Quick Sort"};
 
 int main(int argc, char **argv){
 	int opt = 0;
-	char *seed;
-	int size,elements;
-	
+	uint64_t seed =  13371453;
+        uint32_t size = 100;
+	int elements = 100 ;
+	uint64_t mask = 0x3FFFFFFF;
+	Set command = empty_set();
 	while((opt =getopt(argc, argv , OPTIONS))!=-1){
 		switch(opt){
 			case 'a':
-		        break;
+		        command = insert_set(HEAP, command);
+			command = insert_set(INSERTION, command);
+			command = insert_set(SHELL,command);
+			command = insert_set(QUICK, command);
+			break;
 			
 			case 'e':
-                        break;
+                        command = insert_set(HEAP, command);
+			break;
 			
 			case 'i':
+			command = insert_set(INSERTION, command);
                         break;
  			
 			case 's':
+			command = insert_set(SHELL,command);
                         break;
  			
 			case 'q':
+			command = insert_set(QUICK, command);
                         break;
  			
 			case 'r':
-			seed=argv[optind];
-			
-			
-			
-			printf("%s",seed);
+			seed=mask && atoi(optarg);
 			
                         break;
  			
 			case 'n':
-			size=optopt;
+			size=atoi(optarg);
                         break;
  			
 			case 'p':
-			elements=optopt;	
+			elements=atoi(optarg);	
                         break;
  			
 			case 'h':
                         break;
-		}}
-
+		}
+	}
+	srandom(seed);
+	
 	return 0;
 
-//uint64_t mask = 0x3FFFFFFF;
+
 
 
 
