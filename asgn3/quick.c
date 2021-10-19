1e23e557630f2f89ae751b7fb426c87abc526686
Oliver Hu <oehu@ucsc.edu>
2021-10-19 05:48:44 +0000

    comment

--- asgn3/quick.c
+++ asgn3/quick.c
@@ -3,7 +3,8 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-int partition(Stats *stats, uint32_t *A, int lo, int hi) {
+int partition(Stats *stats, uint32_t *A, int lo,
+    int hi) { //given two int values hi and lo this function iterates through and swaps
     int i = lo - 1;
     for (int j = lo; j < hi; j += 1) {
         if (cmp(stats, A[j - 1], A[hi - 1]) == -1) {
@@ -14,13 +15,14 @@ int partition(Stats *stats, uint32_t *A, int lo, int hi) {
     swap(stats, &A[i], &A[hi - 1]);
     return i + 1;
 }
-void quick_sorter(Stats *stats, uint32_t *A, int lo, int hi) {
+void quick_sorter(Stats *stats, uint32_t *A, int lo,
+    int hi) { //starts with initializing the partition and recursively generating more by calling itself recursively
     if (lo < hi) {
         int p = partition(stats, A, lo, hi);
         quick_sorter(stats, A, lo, p - 1);
         quick_sorter(stats, A, p + 1, hi);
     }
 }
-void quick_sort(Stats *stats, uint32_t *A, uint32_t n) {
+void quick_sort(Stats *stats, uint32_t *A, uint32_t n) { //given the size n of array sorts the array
     quick_sorter(stats, A, 1, n);
 }
--- asgn3/shell.c
+++ asgn3/shell.c
@@ -1,9 +1,9 @@
-#include "insert.h"
+#include "shell.h"
 
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-uint32_t gaps(uint32_t n) {
+uint32_t gaps(uint32_t n) { //sets up a gap generator based off a logrithmic function
     static uint32_t iter_val = 0;
     if (iter_val <= 0) {
         iter_val = (uint32_t)(log(3 + 2 * n) / log(3));
@@ -14,14 +14,18 @@ uint32_t gaps(uint32_t n) {
 }
 void shell_sort(Stats *stats, uint32_t *A, uint32_t n) {
     uint32_t iteration = (uint32_t)(log(3 + 2 * n) / log(3));
-    for (uint32_t i = 0; i < iteration; i += 1) {
+    for (
+        uint32_t i = 0; i < iteration;
+        i
+        += 1) { //iterates starting from first gap and using it to compare and swap element with farther apart elements
         uint32_t gap = gaps(n);
 
         for (uint32_t j = gap; j < n; j += 1) {
             uint32_t h = j;
             uint32_t temp = move(stats, A[j]);
 
-            while ((h >= gap) && cmp(stats, temp, A[h - gap]) == -1) {
+            while ((h >= gap)
+                   && cmp(stats, temp, A[h - gap]) == -1) { //this while loop swaps all the elements
                 A[h] = move(stats, A[h - gap]);
                 h -= gap;
             }
