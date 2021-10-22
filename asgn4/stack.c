e5d27d23930fb942fcffff64ea15906b8e770ed8
Oliver Hu <oehu@ucsc.edu>
2021-10-22 21:15:39 +0000

    fixed bugs

--- asgn4/stack.c
+++ asgn4/stack.c
@@ -1,18 +1,19 @@
-#include "stack.c"
-#include "vertices.h"
+#include "stack.h"
+#include <unistd.h>
+#include <stdlib.h>
 struct Stack {
 uint32_t top; // Index of the next empty slot .
 uint32_t capacity ; // Number of items that can be pushed .
 uint32_t * items ; // Array of items , each with type uint32_t .
 };
 Stack *stack_create(uint32_t capacity){
-	Stack *s = ( Stack *) malloc ( sizeof ( Stack ) ) ;
+	Stack *s = ( Stack *)malloc(sizeof(Stack)) ;
 	if (s) {
-		s- >top = 0;
-		s- > capacity = capacity ;
-		s- > items = ( uint32_t *) calloc ( capacity , sizeof ( uint32_t ) ) ;
-		if (!s- > items ) {
-		free (s) ;
+		s->top = 0;
+		s-> capacity = capacity ;
+		s-> items = ( uint32_t *)calloc( capacity , sizeof ( uint32_t ) ) ;
+		if (!s-> items ) {
+		free(s) ;
 		s = NULL ;
 		}
 	}
@@ -20,9 +21,9 @@ Stack *stack_create(uint32_t capacity){
 }
 
 void stack_delete(Stack **s){
-	if (*s && (*s) ->items ) {
-		free ((*s)->items ) ;
-		free (*s) ;
+	if (*s && (*s)->items ) {
+		free((*s)->items ) ;
+		free(*s) ;
 		*s = NULL ;
 	}
 	return ;
@@ -39,7 +40,7 @@ bool stack_empty(Stack *s){
 }
 
 bool stack_full(Stack *s){
-	return(stack_size(s)==s->capacity)
+	return(stack_size(s)==s->capacity);
 }
 bool stack_push(Stack *s, uint32_t x){
 	if(!stack_full(s)){
@@ -63,21 +64,24 @@ bool stack_pop(Stack *s, uint32_t *x){
 
 bool stack_peek(Stack *s, uint32_t *x){
 	if(!stack_empty(s)){
-                return s->items[s->top];
+		s->top-=1;
+                *x=s->items[s->top];
+		s->top+=1;
+                return true;
         }
         return false;
 
 }
 void stack_copy(Stack *dst, Stack *src){
-	for (int i=0;i<src->top;i+=1){
+	for (uint32_t i=0;i<src->top;i+=1){
 		dst->items[i]=src->items[i];
 	}
 	dst->top=src->top;
 }
 void stack_print(Stack *s, FILE *outfile, char *cities[]){
-	for ( uint32_t i = 0; i < s- >top; i += 1) {
-		fprintf ( outfile , "%s", cities [s- > items [i]]) ;
-		if (i + 1 != s- >top ) {
+	for ( uint32_t i = 0; i < s->top; i += 1) {
+		fprintf ( outfile , "%s", cities [s->items [i]]) ;
+		if (i + 1 != s->top ) {
 			fprintf ( outfile , " -> ") ;
 		}
 	}
