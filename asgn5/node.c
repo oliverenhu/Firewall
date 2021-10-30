0d19176d9f8b8ff363044fba159d06b76a0f76de
Oliver Hu <oehu@ucsc.edu>
2021-10-30 07:46:49 +0000

    node_join

--- asgn5/node.c
+++ asgn5/node.c
@@ -1,13 +1,13 @@
 #include "node.h"
+#include <stdlib.h>
 
-
-Node *node_create(uint8_t symbol, uint64_t frequencyi){
+Node *node_create(uint8_t symbol, uint64_t frequency){
 	Node *n= (Node *) malloc(sizeof(Node));
 	if(n){
 	n->symbol=symbol;
 	n->frequency=frequency;
 
-	{
+	}
 	return n;
 }
 
@@ -15,14 +15,15 @@ Node *node_create(uint8_t symbol, uint64_t frequencyi){
 void node_delete(Node **n){
 	if(*n&&(*n)->left&&(*n)->right){
 	free((*n)->left);
-	free((*n)->right)
+	free((*n)->right);
 	free(*n);
-	*n =NULL
+	*n =NULL;
 	}
 	return;
 }
 Node *node_join(Node *left, Node *right){
-
+	Node *parent=node_create('$',left->frequency + right->frequency);
+	return parent;
 }
 
 void node_print(Node *n);
