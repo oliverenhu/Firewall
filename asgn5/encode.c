36979e0490824109d1cce53583a2272ad247bbdd
Oliver Hu <oehu@ucsc.edu>
2021-11-08 06:43:49 +0000

    fixed

--- asgn5/encode.c
+++ asgn5/encode.c
@@ -11,6 +11,7 @@
 #include <fcntl.h>
 #include "huffman.h"
 #include <inttypes.h>
+#include <sys/stat.h>
 int main(int argc, char **argv) {
     int infile;
     int outfile;
@@ -18,8 +19,8 @@ int main(int argc, char **argv) {
     int opt = 0;
     while ((opt = getopt(argc, argv, "i:o:vh")) != -1) {
         switch (opt) {
-        case 'i': infile = open(optarg, O_RDONLY); break;
-        case 'o': outfile = open(optarg, O_RDWR); break;
+        case 'i': infile = open(optarg, O_RDWR); break;
+        case 'o': outfile = open(optarg, O_RDWR|O_CREAT); break;
         case 'v': stats = true; break;
         case 'h':
             printf("SYNOPSIS\n  Traveling Salesman Problem using DFS.\n\nUSAGE\n  ./tsp [-u] [-v] "
@@ -38,10 +39,16 @@ int main(int argc, char **argv) {
     for (int i = 0; i < bytes; i += 1) {
         
             histogram[buf[i]] += 1;
-  printf("symbol:%c,frequency:%lu\n",buf[i],histogram[buf[i]]);
+            printf("symbol:%c,frequency:%lu\n",buf[i],histogram[buf[i]]);
         
     }
+    int leaves=0;
+    for(int i =0;i<ALPHABET;i+=1){
+    	if(histogram[buf[i]]>0){
+		leaves+=1;
+		}
 
+    }
     histogram[0] += 1;
     histogram[255] += 1;
     	
@@ -54,13 +61,16 @@ int main(int argc, char **argv) {
 
     Header head;
     head.magic=MAGIC;
-    fstat(infile,head.permissions);
-    head.tree_size=leaves*3-1;
+    struct stat permissions;
+  
+    fstat(infile,&permissions);
+    head.permissions=permissions.st_mode;
+    fchmod(outfile,head.permissions);
+    head.tree_size=permissions.st_size;
     head.file_size=bytes;	    
-    write(outfile,header.magic,1);
-    write(outfile,header.tree,1);	 
-    write(outfile,header.tree_size,1)
-    write(outfile,header.file_size,1)
+    write(outfile,&head.magic,4);
+    write(outfile,&head.tree_size,1);
+    write(outfile,&head.file_size,1);
 for(int i = 0; i < bytes; i += 1) {
 	code_print(&table[buf[i]]);
 	printf("\n");
