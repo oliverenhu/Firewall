ee0a779c5ce5564ac9aae3ed546add70573b2681
Oliver Hu <oehu@ucsc.edu>
2021-11-18 21:14:58 +0000

    zero prime

--- asgn6/numtheory.c
+++ asgn6/numtheory.c
@@ -78,7 +78,9 @@ void pow_mod(mpz_t out, mpz_t base, mpz_t exponent, mpz_t modulus){
 	return;
 }
 bool is_prime(mpz_t n, uint64_t iters){
-
+	if(mpz_sgn(n)==0){
+		return false;
+	}	
 	mpz_t r,reven,two,twos,rand,a,y,s1,j,s,nr;
 	mpz_init_set_ui(s,-1);
 	mpz_init_set_ui(two,2);
