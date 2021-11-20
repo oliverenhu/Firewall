42e5c108fc511c84d3c6093164fbe32e36376b5d
Oliver Hu <oehu@ucsc.edu>
2021-11-20 11:45:08 +0000

    working decryptor

--- asgn6/rsa.c
+++ asgn6/rsa.c
@@ -21,6 +21,7 @@ void rsa_make_pub(mpz_t p, mpz_t q, mpz_t n, mpz_t e, uint64_t nbits, uint64_t i
     mpz_sub_ui(pt, p, 1);
     mpz_sub_ui(qt, q, 1);
     mpz_mul(tot, pt, qt);
+    
     do {
         mpz_urandomb(co, state, bit);
         gcd(d, co, tot);
@@ -47,6 +48,7 @@ void rsa_make_priv(mpz_t d, mpz_t e, mpz_t p, mpz_t q) {
     mpz_sub_ui(q1, q, 1);
     mpz_mul(ptot, p1, q1);
     mod_inverse(d, e, ptot);
+  //  gmp_printf("tot %Zd\n",ptot);
     mpz_clears(ptot, p1, q1, NULL);
 }
 void rsa_write_priv(mpz_t n, mpz_t d, FILE *pvfile) {
@@ -90,17 +92,22 @@ void rsa_decrypt(mpz_t m, mpz_t c, mpz_t d, mpz_t n) {
     pow_mod(m, c, d, n);
 }
 void rsa_decrypt_file(FILE *infile, FILE *outfile, mpz_t n, mpz_t d) {
+    size_t j;
     mpz_t k, c, decrypt;
-    mpz_inits(k, c, decrypt);
+    mpz_inits(k, c, decrypt,NULL);
     size_t log = mpz_sizeinbase(n, 2);
     mpz_set_ui(k, log);
     mpz_sub_ui(k, k, 1);
     mpz_fdiv_q_ui(k, k, 8);
     uint8_t *kbytes = (uint8_t *) calloc(mpz_get_ui(k), sizeof(uint8_t));
-    size_t j = gmp_fscanf(infile, "%Zx\n", c);
+    do{
+    j = gmp_fscanf(infile, "%Zx\n", c);
     rsa_decrypt(decrypt, c, d, n);
     mpz_export(kbytes, &j, 1, 1, 1, 0, decrypt);
-    fwrite(kbytes + 1, 1, mpz_get_ui(k) - 1, outfile);
+    fwrite(kbytes + 1, 1, j - 1, outfile);
+    }while(j==mpz_get_ui(k) - 1);
+    mpz_clears(k,c,decrypt,NULL);
+    free(kbytes); 
 }
 void rsa_sign(mpz_t s, mpz_t m, mpz_t d, mpz_t n) {
     pow_mod(s, m, d, n);
