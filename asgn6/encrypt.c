671f9021d1da5e02b2a06fcd25eb5ae9be9ee39f
Oliver Hu <oehu@ucsc.edu>
2021-11-20 10:10:45 +0000

    added -n

--- asgn6/encrypt.c
+++ asgn6/encrypt.c
@@ -18,7 +18,7 @@ int main(int argc, char **argv) {
     FILE *pubkey = stdin;
     bool verbose = false;
     int opt = 0;
-    while ((opt = getopt(argc, argv, "i:o:nvh")) != -1) {
+    while ((opt = getopt(argc, argv, "i:o:n:vh")) != -1) {
         switch (opt) {
         case 'i': infile= fopen(optarg,"r"); break;
         case 'o': outfile= fopen(optarg,"w+"); break;
@@ -31,10 +31,12 @@ int main(int argc, char **argv) {
 	
 	mpz_t usr,s,n,e;
         mpz_inits(usr,s,n,e,NULL);
+	printf("%s",pub);
 	pubkey=fopen(pub,"r");
 	if(pubkey==NULL){
             printf("SYNOPSIS\n   Encrypts data using RSA encryption.\n   Encrypted data is decrypted by the decrypt program.\n\nUSAGE\n   ./encrypt [-hv] [-i infile] [-o outfile] -n pubkey\n\nOPTIONS\n   -h              Display program help and usage.\n   -v              Display verbose program output.\n   -i infile       Input file of data to encrypt (default: stdin).\n   -o outfile      Output file for encrypted data (default: stdout).\n   -n pbfile       Public key file (default: rsa.pub).\n");
-		return 0;
+	    	
+	    return 0;
 	}
 	if(infile==NULL){
 	printf("SYNOPSIS\n   Encrypts data using RSA encryption.\n   Encrypted data is decrypted by the decrypt program.\n\nUSAGE\n   ./encrypt [-hv] [-i infile] [-o outfile] -n pubkey\n\nOPTIONS\n   -h              Display program help and usage.\n   -v              Display verbose program output.\n   -i infile       Input file of data to encrypt (default: stdin).\n   -o outfile      Output file for encrypted data (default: stdout).\n   -n pbfile       Public key file (default: rsa.pub).\n");
