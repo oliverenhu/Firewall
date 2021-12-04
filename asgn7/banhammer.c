3801427535970d50814fef873e8b6d1e8daaf11d
Oliver Hu <oehu@ucsc.edu>
2021-12-04 10:54:35 +0000

    stdint

--- asgn7/banhammer.c
+++ asgn7/banhammer.c
@@ -5,9 +5,12 @@
 # include "bst.h"
 # include "bf.h"
 # include "bv.h"
+# include "salts.h"
+#include <stdint.h>
+#include "speck.h"
 # define WORD "[a-zA-Z]+"
 
-int main ( void ) {
+int main (void){
 	/*
 	regex_t re;
 	if ( regcomp(&re , WORD , REG_EXTENDED ) ) {
@@ -19,13 +22,19 @@ int main ( void ) {
 	while (( word = next_word(stdin,&re))!= NULL ) {
 	printf (" Word : %s\n", word );
 	}*/
-	Node *y=_create("fd","dsf");
+
 	
 		
-//	bst_insert(y,"ewwwe","rer");
 
-	node_print(y);
-	node_delete(&y);
+
+
+	uint32_t d=3011890399;	
+	BloomFilter* b=bf_create(d);
+	bf_insert(b,"apple");
+	
+	printf("%d",bf_probe(b,"apple"));	
+//	bf_print(b);	
+	bf_delete(&b);
 //	clear_words();
 //	regfree(&re);
 	return 0;
